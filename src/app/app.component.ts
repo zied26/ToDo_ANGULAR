b931670a9ff20e1d8588f928a1f103d22ec59177
unknown <ziedmbarki77181@gmail.com>
2022-11-29 13:06:46 +0100

    ngif et ngswitch fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,19 @@ import { Task } from './class/task.model';
 export class AppComponent {
   public  count : number;
   public  tasks : Task[];
-
+  public  completion : number;
   constructor() {
     this.tasks = [
-      new Task(0, "a", true, "e"),
+      new Task(0, "a", false, "e"),
       new Task(1, "b", true, "f"),
       new Task(2, "c", true, "g")
     ];
 
-    this.count = this.tasks.length;
+    this.count = this.tasks.filter(task => task.completed).length;
+    this.completion = (this.count / this.tasks.length)*100;
+    console.log(this.completion);
   }
+
 
   ChangeCount(status : boolean): void {
     this.count = status ? this.count + 1 : this.count - 1;
