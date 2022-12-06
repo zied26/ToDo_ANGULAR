66405261f6b6461c556ac0e884cacce2a4368588
unknown <ziedmbarki77181@gmail.com>
2022-12-06 10:40:32 +0100

    not found + taskDetails + guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './auth-guard.guard';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TaskDetailsComponent } from './components/taskdetails/taskdetails.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [
+const routes: Routes = [  {
+  path: '',
+  canActivate: [AuthGuardGuard],
+  children: [
+    {path: '', component: TodoListComponent, pathMatch: 'full'},
+    {path: 'todolist', component: TodoListComponent, pathMatch: 'full'},
+    {path: 'todolist/:id', component: TaskDetailsComponent},
+  ]
+},
+
   {path: 'login' , component: LoginComponent},
-  {path: 'todo-list' , component: TodoListComponent},
-  {path: '' , component: TodoListComponent, pathMatch: 'full'}
+
+  {path: '404', component: NotFoundComponent},
+
+  {path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  imports: [RouterModule.forRoot(routes/*, {enableTracing: true}*/)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      console.log(this.auth.isAuth);
+      if (!this.auth.isAuth)
+        this.router.navigate(['login']);
+      return (this.auth.isAuth);
+  }
+
+}
